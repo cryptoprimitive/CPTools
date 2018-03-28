b25e4f3ce12ef3fdcfd98ef3b15fd7c5d5f7897d
coinop-logan <coinop.logan@gmail.com>
2018-03-28 09:27:33 +0000

    Don't print output if module

--- cptools.py
+++ cptools.py
@@ -118,16 +118,15 @@ def grantUpdateTo():
 	relock(account)
 
 
-output = """functions:
-newAccount() - Interactively creates a new account, optionally granting it 0.005 ETH of "seed funds" from another account
-printUpdates() - prints all updates from the GrantableUpdates contract
-postUpdate() - interactively posts message to the update feed
-grantUpdateTo() - interactively grant update rights to another address for the GrantableUpdates contract
-"""
-
-print()
-print(output)
-print()
-
 if __name__ == "__main__":
+	output = """functions:
+	newAccount() - Interactively creates a new account, optionally granting it 0.005 ETH of "seed funds" from another account
+	printUpdates() - prints all updates from the GrantableUpdates contract
+	postUpdate() - interactively posts message to the update feed
+	grantUpdateTo() - interactively grant update rights to another address for the GrantableUpdates contract
+	"""
+
+	print()
+	print(output)
+	print()
 	printUpdates()
